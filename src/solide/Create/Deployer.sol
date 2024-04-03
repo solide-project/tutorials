f167847e2318bc0ad120f98d35e06929c7877a61
5208980 <z5208980@zmail.unsw.edu.au>
2024-04-03 21:04:31 +1100

    improve deployer

--- src/solide/Create/Deployer.sol
+++ src/solide/Create/Deployer.sol
@@ -5,21 +5,20 @@ import "solmate/src/utils/CREATE3.sol";
 import "./Ownerable.sol";
 
 contract Deployer {
-    function deploy(bytes memory _salt, uint256 _value) external {
+    function deploy(bytes memory _salt) external payable {
         bytes memory bytecode = abi.encodePacked(
             type(Ownerable).creationCode,
             abi.encode(msg.sender)
         );
 
-        CREATE3.deploy(keccak256(_salt), bytecode, _value);
+        CREATE3.deploy(keccak256(_salt), bytecode, msg.value);
     }
 
-    // function deploy(bytes memory _salt, bytes memory _bc, uint256 _value) external {
+    // function deploy(bytes memory _s`alt, bytes memory _bc) external {
     //     bytes memory bytecode = abi.encodePacked(
     //         _bc,
     //         abi.encode(msg.sender)
     //     );
-
     //     CREATE3.deploy(keccak256(_salt), bytecode, _value);
     // }
 
